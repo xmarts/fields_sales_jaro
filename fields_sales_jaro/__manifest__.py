9c761f3d243879628673efade263d79a8ff05246
Marco <marcocaaguilar@gmail.com>
2019-05-22 12:48:47 -0500

    Creación del manifest

--- fields_sales_jaro/__manifest__.py
+++ fields_sales_jaro/__manifest__.py
@@ -20,16 +20,19 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/fields_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
-}
+    'application' : True,
+    'installable' : True
+} 
