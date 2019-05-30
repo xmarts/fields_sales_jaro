cb35529af4d763ba936b047292a5a70535b807e4
Marco <marcocaaguilar@gmail.com>
2019-05-30 10:27:35 -0500

    Cambio en modelo, relacion ventas

--- fields_sales_jaro/models/models.py
+++ fields_sales_jaro/models/models.py
@@ -9,3 +9,15 @@ class AddFIeldsSalesJaro(models.Model):
 	number_order = fields.Char( string = "Numero de orden" )
 	number_appoi = fields.Char( string = "Numero de cita" )
 	date_of_deli = fields.Date( string = "Fecha de entrega" )
+
+class AddFieldManySales(models.Model):
+
+	_inherit = 'account.invoice'
+
+	field_sales = fields.Many2one( 'sale.order', string = "Campo ventas" , compute = "getValue", readonly = True )
+
+	field_test = fields.Char( string = "Campo de prueba", readonly = True )
+
+	def getValue(self):
+		search = self.env['sale.order'].search([('name', '=', self.origin)], limit = 1)
+		self.field_sales = search.id
